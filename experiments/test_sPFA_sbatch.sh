23b5b2c059feb0ced5890d36cf2102e7a3616f54
Zachary Calhoun <zdc6@dcc-login-03.oit.duke.edu>
2022-06-11 12:34:40 -0400

    Set up environment in shell script.

--- experiments/test_sPFA_sbatch.sh
+++ experiments/test_sPFA_sbatch.sh
@@ -1,12 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
+#SBATCH --output=./slurm.out
+#SBATCH --error=./slurm.err
 
-#SBATCH --output=../results/test/slurm.out
-#SBATCH --error=../results/test/slurm.err
-
-module load Python/3.8.1
-
-python3 s_pfa.py \
+singularity exec -B /hpc/group/carlsonlab/zdc6/ ~/wildfires/wildfire-tweets.sif python3 s_pfa.py \
 	--lemmatized_path /hpc/group/carlsonlab/zdc6/wildfire/data/lemmatized/ \
 	--data_path /hpc/group/carlsonlab/zdc6/sPFA/data/1000_1_by_file/ \
 	--tweet_sample_count 1 \

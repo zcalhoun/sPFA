1df46e9fd165e55a88fe61b10beacf525794a380
Zachary Calhoun <zdc6@dcc-login-02.oit.duke.edu>
2022-07-17 10:08:34 -0400

    Added experiments 32-35

---
+++ experiments/41_1000_10_by_file_35.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --output=./slurm41.out
+#SBATCH --error=./slurm41.err
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --account=carlsonlab
+
+singularity exec --nv -B /hpc/group/carlsonlab/zdc6/ ~/wildfires/wildfire-tweets.sif python3 main.py \
+	--lemmatized_path /hpc/group/carlsonlab/zdc6/wildfire/data/lemmatized/ \
+	--data_path /hpc/group/carlsonlab/zdc6/sPFA/data/1000_10_by_file/ \
+	--tweet_sample_count 10 \
+	--min_df 300 \
+	--max_df .01 \
+	--train_cities chicago dallas los\ angeles new\ york portland san\ francisco phoenix \
+	--test_cities raleigh seattle orange \
+	--nmf_max_iter 300 \
+	--num_components 200 \
+	--l1_reg 20 \
+	--init_kld 0.000001 \
+	--klds_epochs 10 \
+	--mse_weight 10 \
+	--epochs 400 \
+	--pred_dropout 0.5 \
+	--results_path /hpc/group/carlsonlab/zdc6/sPFA/results/exp41/

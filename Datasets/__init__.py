6b094844157542221f8e52b283af1fc6e146ec52
Zach Calhoun <zcalhoun.ny@gmail.com>
2022-12-13 22:00:59 -0500

    New organization (#5)
    In this commit I reorganized the folder organization, and I added a dataset file for the twitter data that is much faster. This should enable much more quick runs of the code to try out different dataset sizes. It should also enable quicker runs of the code in general.

---
+++ Datasets/__init__.py
@@ -0,0 +1,14 @@
+from .twitter import create_dataset
+
+
+def load(
+    data_path,
+    dump_path,
+    num_samples_per_day=1,
+    tweets_per_sample=1000,
+    min_df=0.05,
+    max_df=0.8,
+):
+    return create_dataset(
+        data_path, num_samples_per_day, tweets_per_sample, min_df, max_df, dump_path
+    )

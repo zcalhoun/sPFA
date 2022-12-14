207700a2d2de3b2773e48caf09e614212013d6b9
Zach Calhoun <zcalhoun.ny@gmail.com>
2022-12-13 22:01:30 -0500

    New organization (#5) (#6)
    In this commit I reorganized the folder organization, and I added a dataset file for the twitter data that is much faster. This should enable much more quick runs of the code to try out different dataset sizes. It should also enable quicker runs of the code in general.

---
+++ experiments/main2_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=./main2.out
+#SBATCH --error=./main2.err
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --account=carlsonlab
+
+
+singularity exec --nv -B /hpc/group/carlsonlab/zdc6/,/work/zdc6/ ~/wildfires/wildfire-tweets.sif python3 main_2.py \
+    --data_path /hpc/group/carlsonlab/zdc6/wildfire/data/lemmatized/ \
+    --dump_path /work/zdc6/wildfire/results/test/
